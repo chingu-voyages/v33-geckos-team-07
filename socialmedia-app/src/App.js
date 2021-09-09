0e72e413e80a1d47017a587b1731743097516d42
Charles Ybarra <charles.ybarra@outlook.com>
2021-09-09 17:38:28 -0500

    first commit

---
+++ socialmedia-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from '../src/logo512.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Social-Media App</p>
+        <img src={logo} className="App-logo" alt="logo"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
