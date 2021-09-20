1fa16e5d4894717454ccaa19c4cc7f07d6713d7e
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-09-20 13:19:23 +0100

    Update navbar to fix bugs To fix an error in Firefox, I changed the mobile icon to a Material icon instaed of FontAwesome. I also adjusted the branding/navbar font sizes to be smaller in mobile view.

--- socialmedia-app/src/Components/Header.js
+++ socialmedia-app/src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../logo512.png'
+// import logo from '../logo512.png'
 
 export default function Header() {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -8,52 +8,65 @@ export default function Header() {
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 bg-white">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-auto relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        {/* Website logo */}
+
+                    {/* Website logo */}
                         <a href="/" className="flex items-center px-2">
-                            <img src={logo} className="h-8 w-8 mr-2" alt="logo"></img>
+                            <span className="material-icons h-8 w-8 mr-2">
+                                add_a_photo
+                            </span>
                         </a>
 
                     </div>
 
                     {/* Branding name */}
-                    <div className="flex text-orange items-center font-mono text-5xl xl:pl-16">
-                            <h1>Travelli</h1>
+                    <div className="flex text-orange items-center font-mono lg:text-3xl md:text-xl xs:text-lg xl:pl-16">
+                        <h1>Travelli</h1>
                     </div>
 
-                    <button
-                            className="text-orange cursor-pointer  leading-none px-3 py-1 border-solid border-transparent rounded bg-transparent border block lg:hidden outline-none focus:outline-none"
+                    <div>
+
+                    {/* Mobile nav */}
+                        <button
+                            className="text-orange cursor-pointer  leading-none px-3 py-1 
+                            rounded bg-transparent border block 
+                            lg:hidden outline-none focus:outline-none"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}
                         >
-                            <i className="fas fa-bars"></i>
+                            <span className="material-icons">
+                                reorder
+                            </span>
+
                         </button>
-                    <div
-                        className={
-                            "lg:flex items-center" +
-                            (navbarOpen ? " flex" : " hidden")
-                        }
-                        
-                    >
+                        <div
+                            className={
+                                "lg:flex items-center" +
+                                (navbarOpen ? " flex" : " hidden")
+                            }
 
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                            <li className="nav-item">
-                                <a
-                                    className="px-3 py-2 flex items-center text-orange font-serif text-lg font-semibold"
-                                    href="/"
-                                >
-                                    Login
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className="px-3 py-2 flex items-center items-center text-orange font-serif text-lg font-semibold"
-                                    href="/"
-                                >
-                                    Contact
-                                </a>
-                            </li>
+                        >
 
-                        </ul>
+                    {/* Primary nav links */}
+                            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                                <li className="nav-item">
+                                    <a
+                                        className="px-3 py-2 flex items-center text-orange font-serif lg:text-lg lg:font-semibold md:text-md md:font-normal"
+                                        href="/"
+                                    >
+                                        Login
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="px-3 py-2 flex items-center items-center text-orange font-serif lg:text-lg lg:font-semibold md:text-md md:font-normal"
+                                        href="/"
+                                    >
+                                        Contact
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
